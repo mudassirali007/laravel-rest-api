f4e88882c4a5acc5b7c51c11a34984294eb0ea82
mudassirali007 <mudassiraliansari@gmail.com>
2023-03-19 15:23:12 +0500

    first commit

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class RegisterController extends BaseController
+{
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['name'] =  $user->name;
+        $success['email'] =  $user->email;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+            $success['email'] =  $user->email;
+
+            return $this->sendResponse($success, 'User login successfully.');
+        }
+        else{
+            return $this->sendError('Unauthorized.', ['error'=>'Unauthorized']);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $success['user'] = Auth::user();
+        Auth::user()->tokens()->delete();
+        return $this->sendResponse($success, 'Successfully logged out.');
+    }
+
+    public function user(Request $request)
+        {
+            $success['name'] =  $request->user()->name;
+            $success['email'] =  $request->user()->email;
+
+
+            return $this->sendResponse($success, 'Logged User.');
+        }
+}
